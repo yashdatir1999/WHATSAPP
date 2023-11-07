39b766f59824a69536b1925e21e9f8e087b4ae58
yashdatir1999 <yashdatir1999@gmail.com>
2023-11-07 20:17:43 +0530

    chat started

---
+++ socketapi.js
@@ -0,0 +1,19 @@
+const io = require( "socket.io" )();
+const socketapi = {
+    io: io
+};
+
+// Add your socket.io logic here!
+io.on( "connection", function( socket ) {
+    console.log( "A user connected" );
+
+    socket.on("sending" , (msg) =>{
+        console.log(msg)
+        socket.broadcast.emit("resivemsg" , msg)
+    } )
+});
+// end of socket.io logic
+
+
+
+module.exports = socketapi;
